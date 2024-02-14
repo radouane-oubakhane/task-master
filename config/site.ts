e99f82a67389be43443b8922b1b7153b00d9e14f
radouane <oubakhaneradouane@gmail.com>
2024-02-14 22:35:21 +0100

    Set up Clerk

---
+++ config/site.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  name: "Task-Manger",
+  description:
+    "Collaborate, manage and track your tasks with ease. From high-level planning to the smallest details, we got you covered with our powerfuland easy to use task manager.",
+};
